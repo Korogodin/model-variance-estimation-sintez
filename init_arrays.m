9bd69775e922b0d7024815de6b78af11b490adf6
korogodin_iv@mail.ru <korogodin_iv@mail.ru@6aa776e5-48b3-6f5d-775a-092926ccca4e>
2011-04-23 19:49:30 +0000

    Первый коммит

---
+++ init_arrays.m
@@ -0,0 +1,4 @@
+I = nan(1, Nstdn);
+Q = nan(1, Nstdn);
+X2 = zeros(1,Nstdn);
+X4 = zeros(1,Nstdn);
---
+++ main.m
@@ -0,0 +1,41 @@
+clear all
+clc
+close all
+
+settings;
+
+Nmod = fix(Tmod / Tc); 
+
+stdn_IQ_mean = (stdn_IQ_min + stdn_IQ_max)/2; % Среднее значение
+stdn_IQ = stdn_IQ_min:d_stdn_IQ:stdn_IQ_max; % Массив значений stdn_IQ
+Nstdn = size(stdn_IQ,2); % Число рассматриваемых stdn_IQ
+[A_IQ, qcno] = qcno_change(qcno_dB, stdn_IQ_mean, Tc); % Амплитуда, соответсвующая выбранному qcno_dB
+
+init_arrays;
+phase = rand(1,Nmod)*2*pi;
+for nmod = 1:Nmod
+    n_I = randn(1,Nstdn);
+    n_Q = randn(1,Nstdn);
+    I = A_IQ * cos(phase(nmod)) + n_I.*stdn_IQ;
+    Q = - A_IQ * sin(phase(nmod)) + n_Q.*stdn_IQ;
+    X2 = X2 + I.^2 + Q.^2;    X4 = X4 + (I.^2 + Q.^2).^2;
+end
+
+A_IQ_izm = A_IQ/3;
+SQ_stdn_izm_1 = (X2 + Nmod*A_IQ_izm.^2 + sqrt(X2.^2-6*Nmod*A_IQ_izm.^2.*X2+Nmod^2*A_IQ_izm.^4))/ (4*Nmod);
+SQ_stdn_izm_2 = (X2/Nmod + A_IQ_izm.^2 + sqrt(X4/Nmod-6*A_IQ_izm.^2.*X2/Nmod + A_IQ_izm^4))/ (4);
+SQ_stdn_izm_3 = (X2/Nmod - sqrt(2*(X2/Nmod).^2 - X4/Nmod))/ (2);
+SQ_stdn_izm_4 = (X2/Nmod + A_IQ_izm.^2 - sqrt(2*(X2/Nmod).^2 - X4/Nmod + A_IQ_izm.^4 - 2*A_IQ_izm.^2.*SQ_stdn_izm_3) )/ (2);
+
+hF = 0;
+hF = figure(hF+1);
+plot(stdn_IQ, stdn_IQ, stdn_IQ, sqrt(SQ_stdn_izm_1), stdn_IQ, sqrt(SQ_stdn_izm_2), ...
+    stdn_IQ, sqrt(SQ_stdn_izm_3), stdn_IQ, sqrt(SQ_stdn_izm_4));
+xlabel('stdn_{IQ}');
+ylabel('sqrt SQ\_stdn\_izm');
+
+hF = figure(hF+1);
+plot(stdn_IQ, stdn_IQ.^2, stdn_IQ, SQ_stdn_izm_1, stdn_IQ, SQ_stdn_izm_2, ...
+    stdn_IQ, SQ_stdn_izm_3, stdn_IQ, SQ_stdn_izm_4);
+xlabel('stdn_{IQ}');
+ylabel('SQ\_stdn\_izm');

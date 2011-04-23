3eb98a35b4db01d35303c277af868cae27f530a5
korogodin_iv@mail.ru <korogodin_iv@mail.ru@6aa776e5-48b3-6f5d-775a-092926ccca4e>
2011-04-23 19:49:30 +0000

    Первый коммит

---
+++ trunk/globals.m
@@ -0,0 +1,3 @@
+global Tc Tmod Nmod
+
+global stdn_IQ_min stdn_IQ_max d_stdn_IQ stdn_IQ_mean Nstdn
---
+++ trunk/settings.m
@@ -0,0 +1,13 @@
+globals;
+
+Tc = 0.001; % Correlation period
+
+Tmod = 100.000; % Время моделирования
+
+qcno_dB = 15; % q c/no in dBHz
+
+stdn_IQ_min = 400; % Минимальное значение СКО квадратур
+stdn_IQ_max = 800; % Максимальное --//--
+d_stdn_IQ = 10;  % Шаг по stdn_IQ
+
+

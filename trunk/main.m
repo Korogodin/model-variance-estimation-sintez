350d6e1cf340fed2093ef23bdc429995d373b542
korogodin_iv@mail.ru <korogodin_iv@mail.ru@6aa776e5-48b3-6f5d-775a-092926ccca4e>
2011-05-03 13:17:53 +0000

    Добавлены скрипты расчета апостериорных плотностей вероятности, прочие изменения

--- trunk/main.m
+++ trunk/main.m
@@ -13,6 +13,8 @@ Nstdn = size(stdn_IQ,2); % Число рассматриваемых stdn_IQ
 
 init_arrays;
 phase = rand(1,Nmod)*2*pi;
+A_IQ_izm = A_IQ*1.2;
+
 for nmod = 1:Nmod
     n_I = randn(1,Nstdn);
     n_Q = randn(1,Nstdn);
@@ -21,9 +23,8 @@ for nmod = 1:Nmod
     X2 = X2 + I.^2 + Q.^2;    X4 = X4 + (I.^2 + Q.^2).^2;
 end
 
-A_IQ_izm = A_IQ/3;
-SQ_stdn_izm_1 = (X2 + Nmod*A_IQ_izm.^2 + sqrt(X2.^2-6*Nmod*A_IQ_izm.^2.*X2+Nmod^2*A_IQ_izm.^4))/ (4*Nmod);
-SQ_stdn_izm_2 = (X2/Nmod + A_IQ_izm.^2 + sqrt(X4/Nmod-6*A_IQ_izm.^2.*X2/Nmod + A_IQ_izm^4))/ (4);
+SQ_stdn_izm_1 = (X2 + Nmod*A_IQ_izm.^2 + sqrt(X2.^2 - 6*Nmod*A_IQ_izm.^2.*X2 + Nmod^2*A_IQ_izm.^4))/ (4*Nmod);
+SQ_stdn_izm_2 = (X2/Nmod - A_IQ_izm.^2)/2;
 SQ_stdn_izm_3 = (X2/Nmod - sqrt(2*(X2/Nmod).^2 - X4/Nmod))/ (2);
 SQ_stdn_izm_4 = (X2/Nmod + A_IQ_izm.^2 - sqrt(2*(X2/Nmod).^2 - X4/Nmod + A_IQ_izm.^4 - 2*A_IQ_izm.^2.*SQ_stdn_izm_3) )/ (2);
 
@@ -31,11 +32,13 @@ hF = 0;
 hF = figure(hF+1);
 plot(stdn_IQ, stdn_IQ, stdn_IQ, sqrt(SQ_stdn_izm_1), stdn_IQ, sqrt(SQ_stdn_izm_2), ...
     stdn_IQ, sqrt(SQ_stdn_izm_3), stdn_IQ, sqrt(SQ_stdn_izm_4));
+legend('ist', '1','2','3', '4')
 xlabel('stdn_{IQ}');
 ylabel('sqrt SQ\_stdn\_izm');
 
 hF = figure(hF+1);
 plot(stdn_IQ, stdn_IQ.^2, stdn_IQ, SQ_stdn_izm_1, stdn_IQ, SQ_stdn_izm_2, ...
     stdn_IQ, SQ_stdn_izm_3, stdn_IQ, SQ_stdn_izm_4);
+legend('ist', '1','2','3', '4')
 xlabel('stdn_{IQ}');
 ylabel('SQ\_stdn\_izm');
